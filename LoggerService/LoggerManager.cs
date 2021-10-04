d8eadf8b3b2c296e173b1aaf5285448bc8be00a1
Viktor Kuharenko <71553032+Houkiro@users.noreply.github.com>
2021-10-04 15:22:23 +0300

    Summary
    Изменения 3й главы

--- LoggerService/LoggerManager.cs
+++ LoggerService/LoggerManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace LoggerService
 {
-    class LoggerManager
+    public class LoggerManager : ILoggerManager
     {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+        public LoggerManager()
+        {
+        }
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+        public void LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+        public void LogWarn(string message)
+        {
+            logger.Warn(message);
+        }
     }
 }
